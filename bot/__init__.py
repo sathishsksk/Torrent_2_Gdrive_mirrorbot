2c2e69fc6c22c225734da6075d26c2cc2eab0a96
sathishsksk <67107485+sathishsksk@users.noreply.github.com>
2021-09-02 00:11:34 +0530

    Update __init__.py

--- bot/__init__.py
+++ bot/__init__.py
@@ -266,9 +266,9 @@ except KeyError:
 try:
     IMAGE_URL = getConfig('IMAGE_URL')
     if len(IMAGE_URL) == 0:
-        IMAGE_URL = 'https://telegra.ph/file/019996f816db9ed576cff.jpg'
+        IMAGE_URL = 'https://twitter.com/Sk_mass_king/header_photo'
 except KeyError:
-    IMAGE_URL = 'https://telegra.ph/file/019996f816db9ed576cff.jpg'
+    IMAGE_URL = 'https://twitter.com/Sk_mass_king/header_photo'
 try:
     STOP_DUPLICATE = getConfig('STOP_DUPLICATE')
     if STOP_DUPLICATE.lower() == 'true':
